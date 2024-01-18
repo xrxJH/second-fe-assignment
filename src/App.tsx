7da1ccfd1c06ab6d40a27d5024bfaba020701804
JH <157004345+xrxJH@users.noreply.github.com>
2024-01-19 06:56:06 +0900

    Fix/#5 버그수정 (#6)
    * fix: 두번 눌러야 모달 닫히는 버그 수정
    
    * fix: 두번 눌러야 모달 닫히는 버그 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import router from '@routes/router';
 import { GlobalStyles } from '@styles/globalStyles';
-import { RouterProvider } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import { ThemeProvider } from 'styled-components';
+import { RouterProvider } from 'react-router-dom';
 import { theme } from 'styles/designSystem';
+import {
+  ResetRecoilContext,
+  useResetRecoilByKey,
+} from '@context/resetRecoilContext';
 
 function App() {
+  const { key, updateKey } = useResetRecoilByKey();
+
   return (
     <ThemeProvider theme={theme}>
-      <RecoilRoot>
-        <GlobalStyles />
-        <RouterProvider router={router} />
-      </RecoilRoot>
+      <ResetRecoilContext.Provider value={updateKey}>
+        <RecoilRoot key={key}>
+          <GlobalStyles />
+          <RouterProvider router={router} />
+        </RecoilRoot>
+      </ResetRecoilContext.Provider>
     </ThemeProvider>
   );
 }
