6a56bd3d306925aabfca3e6b60162cc4ee15f33f
second <xrx172@gmail.com>
2024-01-19 23:26:27 +0900

    feat: 헤더에 home이동 기능 추가

--- src/components/pages/order/Header.tsx
+++ src/components/pages/order/Header.tsx
@@ -1,10 +1,16 @@
 import { styled } from 'styled-components';
 import { ReactComponent as LogoSmall } from '@assets/logo-S.svg';
+import { useNavigate } from 'react-router-dom';
+import { PATH } from '@constants/path';
 
 export const TopHeader = () => {
+  const navigate = useNavigate();
+  const handleNavigateToHome = () => {
+    navigate(PATH.HOME);
+  };
   return (
     <Wrapper>
-      <LogoSmall />
+      <LogoSmall onClick={handleNavigateToHome} />
     </Wrapper>
   );
 };
@@ -17,4 +23,8 @@ const Wrapper = styled.div`
   padding: 12px;
   background: ${({ theme: { colors } }) => colors.black};
   box-shadow: 0px 10px 7px -7px rgba(0, 0, 0, 0.25);
+
+  svg {
+    cursor: pointer;
+  }
 `;
